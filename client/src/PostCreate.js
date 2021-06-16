61ffad538d7e2b8e09d89df3a04d6530f15ee742
= <=>
2021-06-16 13:21:43 -0400

    Init project client-posts-comments

---
+++ client/src/PostCreate.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const PostCreate = () => {
+    const [title, setTitle] = useState('')
+
+    const onSubmitPost = async (event) => {
+        event.preventDefault()
+        await axios.post('http://localhost:4000/posts', {
+            title
+        })
+        setTitle('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={e => onSubmitPost(e)}>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default PostCreate
