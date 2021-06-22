bedb730a2825df6e8a5fe450308d8e2500be181d
= <=>
2021-06-21 20:54:17 -0400

    Ingress and Skaffold implementation

--- events/index.js
+++ events/index.js
@@ -11,16 +11,16 @@ app.post('/events', (req, res) => {
     const event = req.body;
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
+    axios.post('http://posts-clusterip-service:4000/events', event).catch((err) => {
         console.log(err.message);
     });
-    axios.post('http://localhost:4001/events', event).catch((err) => {
+    axios.post('http://comments-service:4001/events', event).catch((err) => {
         console.log(err.message);
     });
-    axios.post('http://localhost:4002/events', event).catch((err) => {
+    axios.post('http://queries-service:4002/events', event).catch((err) => {
         console.log(err.message);
     });
-    axios.post('http://localhost:4003/events', event).catch((err) => {
+    axios.post('http://moderation-service:4003/events', event).catch((err) => {
         console.log(err.message);
     });
     res.send({ status: 'OK' });
